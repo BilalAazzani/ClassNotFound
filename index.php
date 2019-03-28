ccdd4bbda6b1a3728f4deab25e2f8c80251f03ff
baazzan <bilalaazzani@hotmail.com>
2019-03-28 09:00:29 +0100

    switch index.php

--- index.php
+++ index.php
@@ -26,6 +26,48 @@ require_once(CHEMIN_VUES.'header.php');
 if(empty($_GET['action'])){
     $_GET['action']='home';
 }
+switch ($_GET['action']) {
+    case 'login': # action=genese
+        require_once(CHEMIN_CONTROLEURS.'LoginController.php');
+        $controller = new AccueilController();
+        break;
+    case 'admin':
+        require_once(CHEMIN_CONTROLEURS.'AdminController.php');
+        $controller = new AdminController($db);
+        break;
+    case 'archive':
+        require_once(CHEMIN_CONTROLEURS.'ArchiveController.php');
+        $controller = new ArchiveController();
+        break;
+    case 'categorie': # action=contact
+        require_once(CHEMIN_CONTROLEURS.'CategorieController.php');
+        $controller = new CategorieController();
+        break;
+    case 'logout': # action=genese
+        require_once(CHEMIN_CONTROLEURS.'LogoutController.php');
+        $controller = new LogoutController();
+        break;
+    case 'membre':
+        require_once(CHEMIN_CONTROLEURS.'MembreController.php');
+        $controller = new MembreController($db);
+        break;
+    case 'question':
+        require_once(CHEMIN_CONTROLEURS.'QuestionController.php');
+        $controller = new QuestionController();
+        break;
+    case 'register': # action=contact
+        require_once(CHEMIN_CONTROLEURS.'RegisterController.php');
+        $controller = new RegisterController();
+        break;
+    case 'utilisateur': # action=contact
+        require_once(CHEMIN_CONTROLEURS.'UtilisateurController.php');
+        $controller = new UtilisateurController();
+        break;
+    default: # Par défaut, le contrôleur de l'accueil est sélectionné
+        require_once(CHEMIN_CONTROLEURS.'AccueilController.php');
+        $controller = new AccueilController();
+        break;
+}
 
 #switch action
 switch ($_GET['action']){
