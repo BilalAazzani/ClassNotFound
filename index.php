71aa37685ed974273cca247c976c6dcad44e5acb
baazzan <bilalaazzani@hotmail.com>
2019-03-28 09:12:11 +0100

    switchhhh

--- index.php
+++ index.php
@@ -10,70 +10,58 @@ define('SESSION_ID',session_id());
 
 
 #Class inclusion
-function loadClasse($class) {
+function loadClass($class) {
     require_once('models/' . $class . '.class.php');
 }
-spl_autoload_register('loadClasse');
+spl_autoload_register('loadClass');
 
 # Data base connection
-require_once(CHEMIN_MODELS . 'db.php');
+require_once(PATH_MODELS . 'Db.class.php');
 $db=Db::getInstance();
 
 #header
-require_once(CHEMIN_VUES.'header.php');
+require_once(PATH_VIEWS.'header.php');
 
 #Default action home
 if(empty($_GET['action'])){
     $_GET['action']='home';
 }
 switch ($_GET['action']) {
-    case 'login': # action=genese
-        require_once(CHEMIN_CONTROLEURS.'LoginController.php');
-        $controller = new AccueilController();
+    case 'login': # action=login
+        require_once(PATH_CONTROLLERS.'LoginController.php');
+        $controller = new HomeController();
         break;
-    case 'admin':
-        require_once(CHEMIN_CONTROLEURS.'AdminController.php');
-        $controller = new AdminController($db);
+    case 'admin': # action = admin
+        require_once(PATH_CONTROLLERS.'AdminController.php');
+        $controller = new AdminController();
         break;
-    case 'archive':
-        require_once(CHEMIN_CONTROLEURS.'ArchiveController.php');
+    case 'archive': # aciton = archive
+        require_once(PATH_CONTROLLERS.'ArchiveController.php');
         $controller = new ArchiveController();
         break;
-    case 'categorie': # action=contact
-        require_once(CHEMIN_CONTROLEURS.'CategorieController.php');
-        $controller = new CategorieController();
+    case 'category': # action= category
+        require_once(PATH_CONTROLLERS.'CategoryController.php');
+        $controller = new CategoryController();
         break;
-    case 'logout': # action=genese
-        require_once(CHEMIN_CONTROLEURS.'LogoutController.php');
+    case 'logout': # action= logout
+        require_once(PATH_CONTROLLERS.'LogoutController.php');
         $controller = new LogoutController();
         break;
-    case 'membre':
-        require_once(CHEMIN_CONTROLEURS.'MembreController.php');
-        $controller = new MembreController($db);
+    case 'member': # action = member
+        require_once(PATH_CONTROLLERS.'MemberController.php');
+        $controller = new MemberController();
         break;
-    case 'question':
-        require_once(CHEMIN_CONTROLEURS.'QuestionController.php');
+    case 'question': # action = question
+        require_once(PATH_CONTROLLERS.'QuestionController.php');
         $controller = new QuestionController();
         break;
-    case 'register': # action=contact
-        require_once(CHEMIN_CONTROLEURS.'RegisterController.php');
+    case 'register': # action=register
+        require_once(PATH_CONTROLLERS.'RegisterController.php');
         $controller = new RegisterController();
         break;
-    case 'utilisateur': # action=contact
-        require_once(CHEMIN_CONTROLEURS.'UtilisateurController.php');
-        $controller = new UtilisateurController();
-        break;
     default: # Par défaut, le contrôleur de l'accueil est sélectionné
-        require_once(CHEMIN_CONTROLEURS.'AccueilController.php');
-        $controller = new AccueilController();
-        break;
-}
-
-#switch action
-switch ($_GET['action']){
-    default:
-        require_once('controllers/HomeController.php');
-        $controller=new HomeController();
+        require_once(PATH_CONTROLLERS.'HomeController.php');
+        $controller = new HomeController();
         break;
 }
 
