8e8220829f2b2e78c50cb59aa1cc557861a55250
Amine-Ayoub <amineayoub.bigham@student.vinci.be>
2019-04-01 09:08:30 +0200

    select & insert questions

--- models/Db.class.php
+++ models/Db.class.php
@@ -27,5 +27,40 @@ class Db{
         }
         return self::$instance;
     }
+
+    public function select_question($keyword=''){
+        if ($keyword != '') {
+            $keyword = str_replace("%", "\%", $keyword);
+            $query = "SELECT * FROM questions WHERE subjet LIKE :keyword COLLATE utf8_bin";
+            $ps = $this->_db->prepare($query);
+            $ps->bindValue(':keyword',"%$keyword%");
+        } else {
+            $query = 'SELECT * FROM questions ';
+            $ps = $this->_db->prepare($query);
+        }
+
+        $ps->execute();
+
+        $tableau = array();
+        while ($row = $ps->fetch()) {
+            $table[] = new Questions($row->id,$row->title,$row->subjet,$row->category,$row->member,$row->creation_date,$row->state,$row->goodanswer);
+        }
+        return $tableau;
+
+    }
+
+    public function insert_question($title,$subject,$category,$member,$creation_date,$state,$goodanswer) {
+        # Solution d'INSERT avec prepared statement
+        $query = 'INSERT INTO questions (title, subject, category, member, creation_date, state, goodanwser) values (:title, :subject, :category, :member, :creation_date, :state, :goodanwser)';
+        $ps = $this->_db->prepare($query);
+        $ps->bindValue(':title',$title);
+        $ps->bindValue(':subject',$subject);
+        $ps->bindValue(':category',$category);
+        $ps->bindValue(':member',$member);
+        $ps->bindValue(':creation',$creation_date);
+        $ps->bindValue(':state',$state);
+        $ps->bindValue(':goodanswer',$goodanswer);
+        return $ps->execute();
+    }
 }
 ?>
