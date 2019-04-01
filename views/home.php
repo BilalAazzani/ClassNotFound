6ae70cc323cfc5ea6ce5fd2b7272241893210a4e
Amine-Ayoub <amineayoub.bigham@student.vinci.be>
2019-04-01 09:52:53 +0200

    search

--- views/home.php
+++ views/home.php
@@ -1,5 +1,11 @@
 <section class="content">
     <h2>All the Questions </h2>
+    <div class="formulaire">
+        <form action="index.php?action=home" method="post">
+            <p>Search : <input type="text" name="keyword" value="<?php echo html_keyword?>"/>
+                <input type="submit" name="form_search" value="Search"></p>
+        </form>
+    </div>
 
     <table>
         <tr>
