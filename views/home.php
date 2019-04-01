0d74c691a684dcc438ed8be4ba4e52a385cc8a17
baazzan <bilalaazzani@hotmail.com>
2019-04-01 10:23:08 +0200

    css3

--- views/home.php
+++ views/home.php
@@ -1,12 +1,13 @@
 <section class="content">
     <h2>All the Questions </h2>
     <div class="formulaire">
-        <form action="index.php?action=home" method="post">
-            <p>Search : <input type="text" name="keyword" value="<?php echo html_keyword?>"/>
-                <input type="submit" name="form_search" value="Search"></p>
+        <form action="../index.php" method="post">
+            <p>
+                <input type="text" name="keyword">
+                <input type="submit" name="form_search" value="Search">
+            </p>
         </form>
     </div>
-
     <table>
         <tr>
             <td width="250px">Vote</td>
