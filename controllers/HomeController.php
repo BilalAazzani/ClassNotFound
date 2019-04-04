591833304e84308b8004a1ec7e175eaa951dc90b
baazzan <bilalaazzani@hotmail.com>
2019-04-04 08:56:20 +0200

    4

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -8,18 +8,32 @@
 
 class HomeController
 {
-    public function __construct()
+    private $_db;
+
+    public function __construct($db)
     {
+        $this->_db = $db;
     }
 
     public function run(){
+        # Mot clé de recherche
+        $html_motcle='';
 
+        # Recherche si un mot clé est entré dans le formulaire form_recherche
+        if (!empty($_POST['form_search'])
+            && !empty($_POST['keyword'])) {
+            $tabquestions=$this->_db->select_question($_POST['keyword']);
+            $html_motcle=htmlspecialchars($_POST['keyword']); # Protection faille XSS pour l'affichage
+        } else {
+            # Sélection de tous les livres sous forme de tableau
+            $tabquestions=$this->_db->select_question();
+        }
 
-
-
-        #view
-        require_once (PATH_VIEWS.'home.php');
+        # Ecrire ici la vue livres.php
+        # $tablivres contient un tableau de livres
+        require_once(PATH_VIEWS . 'home.php');
     }
+
 
 }
 ?>
