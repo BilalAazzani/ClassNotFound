02a742c3e8666356316f749708294bac286588fa
Amine-Ayoub <amineayoub.bigham@student.vinci.be>
2019-04-18 13:34:34 +0200

    login test1

---
+++ controllers/CreateQuestionController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Amine-Ayoub
+ * Date: 18/04/2019
+ * Time: 13:32
+ */
+
+class CreateQuestionController
+{
+    public function __construct()
+    {
+    }
+
+    public function run(){
+            require_once(PATH_VIEWS . 'question.php');
+
+    }
+}
+?>
