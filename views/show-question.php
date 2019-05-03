698fe10ff4ef58f2bab899a6a1de1b15fcba3f8e
Dustipl <aminebigham@gmail.com>
2019-05-03 19:12:58 +0200

    Answer anonyme

--- views/show-question.php
+++ views/show-question.php
@@ -1,5 +1,5 @@
 <section class="content">
-    <div class="row">
+    <div class="card-body">
         <div class="col-12">
             <h1 class="text-center"><?php echo $question->title ?></h1>
 
@@ -27,10 +27,10 @@
                         </form>
                     <?php }elseif ($question->state=='S'){?>
                         <div class="form_solved">
-                        <form action="index.php?action=state-change" method="post">
-                            <button type="submit" name="form_open_question" value="O" class="btn btn-success"> <i class="fas fa-lock-open"></i>Mark as open</button>
-                            <input type="hidden" name="question_id_open" value="<?php echo $question->question_id ?>">
-                        </form>
+                            <form action="index.php?action=state-change" method="post">
+                                <button type="submit" name="form_open_question" value="O" class="btn btn-success"> <i class="fas fa-lock-open"></i>Mark as open</button>
+                                <input type="hidden" name="question_id_open" value="<?php echo $question->question_id ?>">
+                            </form>
                         </div>
                     <?php } ?>
                 <?php } } ?>
@@ -47,59 +47,56 @@
     </div>
 
     <!--- Answers area -->
-    <div class="row">
-        <?php
-        foreach ($answers as $answer) {
-            ?>
-            <div class="col-4">
-                <i class="fa fa-user"></i> <?php echo $answer->first_name . ' ' . $answer->last_name; ?>
-                <p><small> <i class="fa fa-calendar"></i> <?php echo date_format(date_create($answer->creation_date), 'd/m/Y H:i:s') ?></small></p>
-            </div>
-
-            <div class="col-8 alert alert-info ">
-                <?php echo $answer->subject; ?>
-
-                <!--- Votes -->
-                <div class="vote">
-
-                    <form action="index.php?action=vote" method="post">
-                        <button type="submit" name="form_vote" value="p" class="btn btn-success"><i class="fa fa-plus"></i></button>
-                        <span>
-                        <?php echo $answer->totalVote ?>
-                        </span>
-                        <button type="submit" name="form_vote" value="n" class="btn btn-danger"><i class="fa fa-minus"></i></button>
-                        <input type="hidden" value="<?php echo $answer->answer_id ?>" name="answer_id">
-                        <input type="hidden" name="question_id_vote" value="<?php echo $question->question_id ?>">
-
-                    </form>
-                </div>
-
-            </div>
-
+    <div class="card-body">
+        <div class="row">
             <?php
-        }
-        ?>
-    </div>
-
-    <!--- Add an answer area -->
-    <?php if(empty($_SESSION['authenticated'])){ ?>
-        <p align="center">You must be logged in to answer</p>
-        <p align="center">
-            <a href="index.php?action=login">Log in</a>
-            or
-            <a href="index.php?action=register">Register</a>
-        </p>
-    <?php }else { ?>
-        <div>
-            <form action="index.php?action=insert-answer" method="post">
-                <div class="form-group">
-                    <textarea name="subject" class="form-control" id="subject" rows="2" placeholder="Write your answer"></textarea>
+            foreach ($answers as $answer) {
+                ?>
+                <div class="col-4">
+                    <i class="fa fa-user"></i> <?php echo $answer->first_name . ' ' . $answer->last_name; ?>
+                    <p><small> <i class="fa fa-calendar"></i> <?php echo date_format(date_create($answer->creation_date), 'd/m/Y H:i:s') ?></small></p>
                 </div>
-                <input type="hidden" name="question_id" value="<?php echo $question->question_id ?>">
-                <p class="text-center">
-                    <button type="submit" name="form_insert_answer" class="btn btn-primary"><i class="fa fa-plus"></i>Answer</button>
-                </p>
-            </form>
+
+                <div class="col-8 alert alert-info ">
+                    <?php echo $answer->subject; ?>
+
+                    <!--- Votes -->
+                    <div class="vote">
+
+                        <form action="index.php?action=vote" method="post">
+                            <button type="submit" name="form_vote" value="p" class="btn btn-success"><i class="fa fa-plus"></i></button>
+                            <span>
+                                <?php echo $answer->totalVote ?>
+                            </span>
+                            <button type="submit" name="form_vote" value="n" class="btn btn-danger"><i class="fa fa-minus"></i></button>
+                            <input type="hidden" value="<?php echo $answer->answer_id ?>" name="answer_id">
+                            <input type="hidden" name="question_id_vote" value="<?php echo $question->question_id ?>">
+
+                        </form>
+                    </div>
+
+                </div>
+
+                <?php
+            }
+            ?>
         </div>
-    <?php } ?>
+
+        <!--- Add an answer area -->
+        <div class="notification">
+            <p align="center"><?php echo $notification ?></p>
+        </div>
+
+            <div>
+                <form action="index.php?action=insert-answer" method="post">
+                    <div class="form-group">
+                        <textarea name="subject" class="form-control" id="subject" rows="2" placeholder="Write your answer"></textarea>
+                    </div>
+                    <input type="hidden" name="question_id" value="<?php echo $question->question_id ?>">
+                    <p class="text-center">
+                        <button type="submit" name="form_insert_answer" class="btn btn-primary"><i class="fa fa-plus"></i>Answer</button>
+                    </p>
+                </form>
+            </div>
+    </div>
 </section>
